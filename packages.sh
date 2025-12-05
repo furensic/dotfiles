def311b36dc5894e353eb6c1f00d23f8fc19e885
Leo <info@furensic.eu>
2025-12-05 23:03:48 +0100

    updated cargo installation method for eza

--- packages.sh
+++ packages.sh
@@ -8,5 +8,4 @@ packages=(atuin zsh zoxide tmux wget curl openssh duf du-dust fzf neovim vim go)
 sudo dnf install -y ${packages[@]}
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source .bashrc # cargo isnt immediately recognized in the PATH
-cargo install eza
+~/.cargo/bin/cargo install eza
