b5b3cb9d4ce27e3b63fba676e24c001276c84811
Leo <info@furensic.eu>
2025-12-05 22:49:08 +0100

    added cargo installation, eza installation

--- packages.sh
+++ packages.sh
@@ -3,6 +3,10 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-packages=(zsh zoxide rust tmux wget curl openssh duf du-dust fzf neovim vim go)
+packages=(zsh zoxide tmux wget curl openssh duf du-dust fzf neovim vim go)
 
 sudo dnf install -y ${packages[@]}
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source .bashrc # cargo isnt immediately recognized in the PATH
+cargo install eza
