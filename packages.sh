39f5a1f55d553be38b79620f9aa1913965aae737
Leo <info@furensic.eu>
2025-12-05 23:05:19 +0100

    added stow to packages

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-packages=(atuin zsh zoxide tmux wget curl openssh duf du-dust fzf neovim vim go)
+packages=(stow atuin zsh zoxide tmux wget curl openssh duf du-dust fzf neovim vim go)
 
 sudo dnf install -y ${packages[@]}
 
